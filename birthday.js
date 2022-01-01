002fbdbe98579b99bf61d87a585c22c171bb8beb
PH03be <phoebeleong8@gmail.com>
2022-01-02 08:18:34 +1100

    moved birthday script to birthday.js

---
+++ birthday.js
@@ -0,0 +1,17 @@
+function getAge()
+{
+    const date = new Date()
+
+    // Minus 2007 (ph3b3's birth year) from the current year to get phebe's theoretical current age
+    const age = date.getUTCFullYear() - 2007
+
+    // If September 26th (phebe's birthday) has not passed, minus phebe's age by one and return that value
+    if (date.getUTCMonth() < 9 && date.getUTCDay() < 26) return age - 1;
+    else return age;
+}
+
+// Sets phebe's age on the "General information about ph3b3" section
+window.onload = function()
+{
+    document.getElementById("age").innerHTML = "Age: " + getAge();
+}
