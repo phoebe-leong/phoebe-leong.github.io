f2a4a32f64e4da2da283d2411b249e0855da8c00
ph3b3 <dariusleong8@gmail.com>
2023-09-26 01:15:18 +1000

    fix birthday.js
    my bday happened today and it didnt work :<

--- birthday.js
+++ birthday.js
@@ -1,14 +1,15 @@
-function getAge() {
+    function getAge() {
     const date = new Date()
 
     // Minus 2007 (ph3b3's birth year) from the current year to get phebe's theoretical current age
     const age = date.getUTCFullYear() - 2007
 
-    // If September 26th (phebe's birthday) has not passed, minus phebe's age by one and return that value
-    if (date.getUTCMonth() < 9 && date.getUTCDay() < 26) return age - 1;
+    // If September 26th (phebe's birthday) has not passed, minus phebe's age by one and return that value (date.getUTCMonth() starts at 0)
+    if (date.getUTCMonth() + 1 < 9 && date.getDate() < 26) return age - 1;
     else return age;
 }
 
 window.onload = () => {
     document.getElementById("age").innerHTML = "Age: " + getAge();
+    console.log(getAge())
 }
